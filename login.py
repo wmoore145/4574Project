3014876ce9a2bf09139b34d54b5223f013015a9f
ADF-00 <stewartsadf@vt.edu>
2022-10-24 17:30:55 -0400

    Added Login and Basic QT App

---
+++ login.py
@@ -0,0 +1,20 @@
+from pymongo import MongoClient
+
+def login(username, password):
+    mongo_client = MongoClient('mongodb://localhost:27017')#assuming local database
+    col = mongo_client["user_data"]["user_data"]#database user_data and collection user_data
+    query = col.find_one({"username": username, "password": password})
+    if query is None:
+        return False
+    return True
+
+
+def register(username, password):
+    mongo_client = MongoClient('mongodb://localhost:27017')#assuming local database
+    col = mongo_client["user_data"]["user_data"]#database user_data and collection user_data
+    query = col.find_one({"username": username})
+    if query is not None:
+        return False #username already taken
+    col.insert_one({"username": username, "password": password})
+    return True
+    

f43ba0619e68ceaed429acbc8e5b1354759400a3
kevinvo2000 <77683373+kevinvo2000@users.noreply.github.com>
2022-11-26 17:55:10 -0500

    Travel Time(Not sure where to add method)
    Added Travel Time. Not sure where to put Travel time, so I put it in here for now.

--- appClass.py
+++ appClass.py
@@ -1,10 +1,15 @@
+import simplejson, urllib
+
+
 
 class AppointmentSlot:
-    def __init__(self, starttime, endtime, book=False,n = None):
+    def __init__(self, starttime, endtime, book=False,n = None, origin_lat = None, origin_long = None, final_lat = None, final_long = None):
         self.start = starttime
         self.end = endtime
         self.booked = book
         self.name = n
+        self.origin = origin_lat,origin_long
+        self.finaldest = final_lat,final_long
 
     def get_start(self):
         return self.start
@@ -24,6 +29,18 @@ class AppointmentSlot:
     #for canceling appointment
     def set_booked(self, b):
         self.booked = b
+        
+    def set_origin(origin_lat, origin_long):
+        self.origin = origin_lat,origin_long
+    
+    def set_finalDestination(final_lat, final_long):
+        self.finaldest = final_lat,final_long
+    
+    def get_travel_time():
+        url = "http://maps.googleapis.com/maps/api/distancematrix/json?origins={0}&destinations={1}&mode=driving&language=en-EN&sensor=false".format(str(self.origin),str(self.finaldest))
+        result= simplejson.load(urllib.urlopen(url))
+        driving_time = result['rows'][0]['elements'][0]['duration']['value']
+        print(driving_time)
 
 
 class CSlot(AppointmentSlot):
